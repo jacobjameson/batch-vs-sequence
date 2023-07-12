9d5eb98308d82b329cc0c78db1b1a086dd9f7001
jacobjameson <jacobjameson@g.harvard.edu>
2023-07-12 18:10:36 -0400

    working on figure 1

--- src/figures.R
+++ src/figures.R
@@ -6,15 +6,46 @@ rm(list = ls())
 
 source('src/main.R')
 
+library(grid)
+library(shadowtext)
+library(GGally)
+library(network)
+library(sna)
+library(tidyverse)
+library(ggnetwork)
+library(intergraph)
+library(igraph)
+library(circlize)
+library(caret)
+library(sandwich)
+
 #=========================================================================
 # Figure 1
-#   - Variability in batching vs sequencing within most common complaints
+#   - Show that across physician, there is random assignment and patient
+#     characteristics are balanced
 #=========================================================================
 
-ed_data_variability <- final %>%
-  group_by(CHIEF_COMPLAINT) %>%
-  summarise(variance = var(any.batch, na.rm = TRUE)) %>%
-  arrange(desc(variance)) %>%
-  top_n(10, variance)
+dummy <- dummyVars(" ~ CHIEF_COMPLAINT", data=final)
+one.hot <- data.frame(predict(dummy, newdata=final))
+final <- cbind(final, one.hot)
 
-print(ed_data_variability)
+
+complaints <-  data.frame(Df = numeric(), F = numeric(), 
+                          Pr..F. = numeric(), complaint = character(),
+                          stringsAsFactors = FALSE)
+
+for (complaint in names(one.hot)){
+
+  model_pos_1 <- lm(as.formula(paste(complaint, '~ 1')), final)
+  model_pos_2 <- lm(as.formula(paste(complaint, '~ ED_PROVIDER')), final)
+                    
+  wald_pos <- lmtest::waldtest(model_pos_1, model_pos_2, 
+                               vcov = vcovHC(model_pos_2, type = "HC1"))
+  print(wald_pos)
+  temp <- data.frame(wald_pos)[2, c(2,3,4)]
+  temp$complaint <- complaint
+  
+  complaints <- rbind(complaints, temp)
+  
+}
+
